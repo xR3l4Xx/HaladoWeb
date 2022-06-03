6bf78707ad7918b8fa6a08fd7d749ab14b6e7078
ricsi <richard.kiss@eostryx.com>
2022-06-03 05:56:04 +0200

    resolve site names

--- src/app/features/wagon/pages/wagons-list/wagons-list.component.ts
+++ src/app/features/wagon/pages/wagons-list/wagons-list.component.ts
@@ -1,7 +1,8 @@
 import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 import { NgbModal } from '@ng-bootstrap/ng-bootstrap';
-import { Wagon } from 'app/core/models';
+import { Site, Wagon } from 'app/core/models';
 import { DataStore } from 'app/core/stores';
+import { SiteService } from 'app/features/site/services/site.service';
 import { takeUntil, map, filter } from 'rxjs';
 import { CreateWagonComponent } from '../../components/create-wagon/create-wagon.component';
 import { EditWagonComponent } from '../../components/edit-wagon/edit-wagon.component';
@@ -16,9 +17,11 @@ import { EditWagonComponent } from '../../components/edit-wagon/edit-wagon.compo
 export class WagonsListComponent implements OnInit {
 
   wagons: Wagon[] = [];
+  sites!: Site[];
   showDeleted: boolean = false;
 
-  constructor(public store: DataStore, public modalService: NgbModal) { }
+  constructor(public store: DataStore, public modalService: NgbModal,
+    public siteService: SiteService) { }
 
   onAddWagon(wagon: Wagon) {
     this.store.addWagon(wagon)
@@ -35,6 +38,11 @@ export class WagonsListComponent implements OnInit {
       siteId: 1
     };
     modalRef.componentInstance.saveWagon = (wagon: Wagon) => this.onAddWagon(wagon);
+  }
+
+  inferSiteName(siteId: number) {
+    const site = this.sites.find(s => s.id == siteId);
+    return site?.name;
   }
 
   onUpdateWagon(wagon: Wagon) {
@@ -74,6 +82,7 @@ export class WagonsListComponent implements OnInit {
 
   ngOnInit(): void {
     this.updateFilter();
+    this.siteService.fetchAllSites((sites: Site[]) => this.sites = sites)
   }
 
 }
